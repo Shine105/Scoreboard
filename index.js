239a93b394ba5496049903624fd572ca9f2bf1c5
Shine105 <simranshine105@gmail.com>
2023-07-13 12:10:16 +0530

    It's a counter

---
+++ index.js
@@ -0,0 +1,41 @@
+let Num1 = document.getElementById("Num-1")
+console.log(Num1)
+
+ 
+ let count = 0
+
+function add1() {
+    count += 1
+    Num1.textContent = count
+    }
+
+function add2() {
+    count += 2
+    Num1.textContent = count 
+    }
+
+function add3() {
+    count += 3
+    Num1.textContent = count 
+    }
+    
+    // For guest
+  let Num2 = document.getElementById("Num-2")
+  console.log(Num2) 
+  
+  let guest = 0
+  
+    function addd1() {
+    guest += 1
+    Num2.textContent = count
+    }
+
+function addd2() {
+    guest += 2
+    Num2.textContent = count 
+    }
+
+function addd3() {
+    guest += 3
+    Num2.textContent = count 
+    }
